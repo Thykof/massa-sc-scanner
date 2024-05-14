a050984441ff1c01e3e95361990c1097530b9a51
Nathan Seva <thykof@protonmail.ch>
2024-05-13 19:08:05 -0500

    improve scanner, add verifier

---
+++ smart-contract/src/read-storage.ts
@@ -0,0 +1,31 @@
+import { bytesToStr, bytesToU256, bytesToU64 } from '@massalabs/massa-web3';
+import { getClient } from './lib';
+import { config } from 'dotenv';
+config();
+
+const targetAddress = 'AS';
+
+const { client } = await getClient(process.env.WALLET_SECRET_KEY!);
+
+const addressInfo = await client.publicApi().getAddresses([targetAddress]);
+const keys = addressInfo[0].candidate_datastore_keys;
+for (const k of keys) {
+  const key = Uint8Array.from(k);
+  console.log('--------------------------');
+  console.log('key: ', key);
+  console.log('key (string): ', bytesToStr(key));
+  const entry = await client.publicApi().getDatastoreEntries([
+    {
+      address: targetAddress,
+      key: key,
+    },
+  ]);
+  const value = entry[0].candidate_value;
+  console.log('  entry string: ', bytesToStr(value!));
+  try {
+    console.log('  entry u256: ', bytesToU256(value!));
+  } catch (error) {}
+  try {
+    console.log('  entry u64: ', bytesToU64(value!));
+  } catch (error) {}
+}
