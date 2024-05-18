5991933852790937a282e38b271de1879afbd0b8
Nathan Seva <thykof@protonmail.ch>
2024-05-18 09:23:39 -0500

    front: fix init default client

--- frontend/src/hooks/client.ts
+++ frontend/src/hooks/client.ts
@@ -12,22 +12,22 @@ export function useClient() {
   const { massaClient, chainId } = useAccountStore();
   const [publicClient, setPublicClient] = useState<Client>();
 
-  useEffect(() => {
-    import.meta.env.VITE_CHAIN_ID
-      ? ClientFactory.createDefaultClient(
-          DefaultProviderUrls.BUILDNET,
-          BUILDNET_CHAIN_ID,
-        ).then(setPublicClient)
-      : ClientFactory.createDefaultClient(
-          DefaultProviderUrls.MAINNET,
-          MAINNET_CHAIN_ID,
-        ).then(setPublicClient);
-  }, []);
-
   const isMainnet =
     chainId === undefined
       ? import.meta.env.VITE_CHAIN_ID === MAINNET_CHAIN_ID.toString()
       : chainId === MAINNET_CHAIN_ID;
+
+  useEffect(() => {
+    isMainnet
+      ? ClientFactory.createDefaultClient(
+          DefaultProviderUrls.MAINNET,
+          MAINNET_CHAIN_ID,
+        ).then(setPublicClient)
+      : ClientFactory.createDefaultClient(
+          DefaultProviderUrls.BUILDNET,
+          BUILDNET_CHAIN_ID,
+        ).then(setPublicClient);
+  }, [isMainnet]);
 
   const contractAddressScanner = isMainnet
     ? import.meta.env.VITE_SC_ADDRESS_SCANNER_MAINNET
