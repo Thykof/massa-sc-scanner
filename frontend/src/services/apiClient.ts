64bb13d40bb7fc301597b4aa968a595c25df31eb
Nathan Seva <thykof@protonmail.ch>
2024-05-20 17:52:44 -0500

    front: download zip file

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // Adjust the baseURL as needed
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+export const fetchFile = async (address: string): Promise<Blob> => {
+  const response = await axios.get(`${address}/download`, {
+    responseType: 'blob',
+  });
+
+  return response.data;
+};
