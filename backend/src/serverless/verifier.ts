df39739dc42824cb25b10342eb2662cb0ec7b5a0
Nathan Seva <thykof@protonmail.ch>
2024-05-31 20:39:08 -0500

    add serverless

---
+++ backend/src/serverless/verifier.ts
@@ -0,0 +1,62 @@
+import { Handler, APIGatewayProxyEvent } from 'aws-lambda';
+import { downloadZip, verified, verify } from 'src/services/verifier';
+import * as parser from 'lambda-multipart-parser';
+import { config } from 'dotenv';
+import { headers } from './common';
+import { ZIP_MIME_TYPE } from 'src/const';
+config();
+
+export const handler: Handler = async (event: APIGatewayProxyEvent) => {
+  console.log('received event');
+
+  const address = event.queryStringParameters?.address;
+  const chainId = event.queryStringParameters?.chainId;
+  const path = event.path;
+
+  switch (path) {
+    case '/verified': // http://localhost:3000/dev/verified?address=AS1pBvN6cMKEEBwFfpQNmfY6hT85XcL2VzJSuAFeHt5Ybvbo7mb
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(await verified(address)),
+      };
+    case '/verify':
+      const result = await parser.parse(event);
+      if (result.files.length === 0) {
+        return {
+          statusCode: 400,
+          headers,
+          body: JSON.stringify({ message: 'No file uploaded' }),
+        };
+      }
+      const file = result.files[0];
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(
+          await verify(result.address, result.chainIdString, {
+            buffer: file.content,
+            size: file.content.length,
+            mimetype: file.contentType,
+          }),
+        ),
+      };
+    case '/zip':
+      return {
+        statusCode: 200,
+        headers: {
+          ...handler,
+          'Content-Disposition': `attachment; filename="${address}.zip"`,
+          'Content-Type': ZIP_MIME_TYPE,
+        },
+        body: (await downloadZip(address, chainId)).toString('base64'),
+        isBase64Encoded: true,
+      };
+    default:
+      return {
+        statusCode: 404,
+        headers,
+        body: JSON.stringify({ message: 'Not Found' }),
+      };
+  }
+};
