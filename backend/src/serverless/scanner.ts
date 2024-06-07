7d2f63b9a7b73d8e40916e952e4e9c6dc5bd61c1
Nathan Seva <thykof@protonmail.ch>
2024-06-07 16:38:45 -0500

    improve scan-from-massexplo

--- backend/src/serverless/scanner.ts
+++ backend/src/serverless/scanner.ts
@@ -17,46 +17,55 @@ export const handler: Handler = async (event: APIGatewayProxyEvent) => {
   const opId = event.queryStringParameters?.opId;
   const path = event.path;
 
-  switch (path) {
-    case '/scanner':
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify(await scanSmartContract(address, chainId)),
-      };
-    case 'scan-from-massexplo':
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify(await scanFromMassexplo(opId)),
-      };
-    case '/wat':
-      return {
-        statusCode: 200,
-        headers: {
-          ...headers,
-          'Content-Disposition': `attachment; filename="${address}.wat"`,
-          'Content-Type': 'text/plain',
-        },
-        body: (await downloadWat(address, chainId)).toString('base64'),
-        isBase64Encoded: true,
-      };
-    case '/wasm':
-      return {
-        statusCode: 200,
-        headers: {
-          ...headers,
-          'Content-Disposition': `attachment; filename="${address}.wasm"`,
-          'Content-Type': 'application/octet-stream',
-        },
-        body: (await downloadWasm(address, chainId)).toString('base64'),
-        isBase64Encoded: true,
-      };
-    default:
-      return {
-        statusCode: 404,
-        headers,
-        body: JSON.stringify({ message: 'Not Found' }),
-      };
+  try {
+    switch (path) {
+      case '/scanner':
+        return {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify(await scanSmartContract(address, chainId)),
+        };
+      case '/scan-from-massexplo':
+        return {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify(await scanFromMassexplo(opId, chainId)),
+        };
+      case '/wat':
+        return {
+          statusCode: 200,
+          headers: {
+            ...headers,
+            'Content-Disposition': `attachment; filename="${address}.wat"`,
+            'Content-Type': 'text/plain',
+          },
+          body: (await downloadWat(address, chainId)).toString('base64'),
+          isBase64Encoded: true,
+        };
+      case '/wasm':
+        return {
+          statusCode: 200,
+          headers: {
+            ...headers,
+            'Content-Disposition': `attachment; filename="${address}.wasm"`,
+            'Content-Type': 'application/octet-stream',
+          },
+          body: (await downloadWasm(address, chainId)).toString('base64'),
+          isBase64Encoded: true,
+        };
+      default:
+        return {
+          statusCode: 404,
+          headers,
+          body: JSON.stringify({ message: 'Not Found' }),
+        };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ message: 'Internal error' }),
+    };
   }
 };
